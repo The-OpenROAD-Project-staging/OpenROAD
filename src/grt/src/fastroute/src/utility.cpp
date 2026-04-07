a83062ead64597c02dc935c2789f918fa6e4f07d
Jonas Gava <jfgava@precisioninno.com>
2026-04-07 17:29:22 +0000

    grt: new weights for res-aware scoring
    Signed-off-by: Jonas Gava <jfgava@precisioninno.com>

--- src/grt/src/fastroute/src/utility.cpp
+++ src/grt/src/fastroute/src/utility.cpp
@@ -95,10 +95,10 @@ void FastRouteCore::ConvertToFull3DType2()
 // Resistance-aware score calculation to order critical nets
 float FastRouteCore::getResAwareScore(FrNet* net)
 {
-  const float kResistanceWeight = 2.0f;
-  const float kSlackWeight = 4.0f;
-  const float kFanoutWeight = 3.0f;
-  const float kNetLengthWeight = 1.0f;
+  const float kResistanceWeight = 1.0f;
+  const float kSlackWeight = 5.0f;
+  const float kFanoutWeight = 2.0f;
+  const float kNetLengthWeight = 3.0f;
 
   return kResistanceWeight * (net->getResistance() / worst_net_resistance_)
          + kSlackWeight * (net->getSlack() / worst_slack_)
