6a4ca2212fc2004dfd18a8f2dca21da7bd011218
Jonas Gava <jfgava@precisioninno.com>
2026-04-07 17:28:26 +0000

    rsz: new sequence of moves for res-aware strategy
    Signed-off-by: Jonas Gava <jfgava@precisioninno.com>

--- src/rsz/src/RepairSetup.cc
+++ src/rsz/src/RepairSetup.cc
@@ -1417,18 +1417,22 @@ bool RepairSetup::repairPathResAware(sta::Path* path,
     std::vector<BaseMove*> moves;
     if (phase == ResAwarePhase::WIRE) {
       moves = {resizer_->res_aware_move_.get(),
+               resizer_->unbuffer_move_.get(),
                resizer_->vt_swap_speed_move_.get(),
                resizer_->size_up_move_.get(),
+               resizer_->swap_pins_move_.get(),
                resizer_->buffer_move_.get(),
-               resizer_->split_load_move_.get(),
-               resizer_->clone_move_.get()};
+               resizer_->clone_move_.get(),
+               resizer_->split_load_move_.get()};
     } else {
       moves = {resizer_->unbuffer_move_.get(),
                resizer_->vt_swap_speed_move_.get(),
                resizer_->size_up_move_.get(),
                resizer_->swap_pins_move_.get(),
                resizer_->res_aware_move_.get(),
-               resizer_->clone_move_.get()};
+               resizer_->buffer_move_.get(),
+               resizer_->clone_move_.get(),
+               resizer_->split_load_move_.get()};
     }
 
     // Try moves on drivers sorted by descending delay.
